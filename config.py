185d743470846172ddd5094008c1c08193f397c4
ronserruya <ron.serruya@kik.com>
2018-01-14 15:03:30 +0200

    Initial Commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import sys
+
+# Private info from command line args
+username = "CryptoNotifier"
+password = sys.argv[1]
+client_secret = sys.argv[2]
+client_id = sys.argv[3]
+developer = sys.argv[4]
